10efee9cc8d10b840d377beeb309105f3dea8988
Philippe Jacquier <phil.jacquier@hotmail.fr>
2022-12-07 17:43:02 +0100

    Dernières modifs

--- js/blocs.js
+++ js/blocs.js
@@ -188,7 +188,13 @@ function addLightBoxSwipeSupport() {
         threshold: 0
     })
 }
-$(window).load(function () {
+$(document).ready(function () {
+    $("#scroll-hero").click(function (t) {
+        t.preventDefault(), $("html,body").animate({
+            scrollTop: $("#scroll-hero").closest(".bloc").height()
+        }, "slow")
+    }), extraNavFuncs(), setUpSpecialNavs(), setUpDropdownSubs(), setUpLightBox(), setUpVisibilityToggle(), addSwipeSupport(), addKeyBoardSupport(), -1 != navigator.userAgent.indexOf("Safari") && -1 == navigator.userAgent.indexOf("Chrome") && $("#page-loading-blocs-notifaction").remove()
+}), $(window).load(function () {
     setFillScreenBlocHeight(), animateWhenVisible(), $("#page-loading-blocs-notifaction").remove()
 }).resize(function () {
     setFillScreenBlocHeight()
